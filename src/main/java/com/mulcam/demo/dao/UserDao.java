df6a7633a7193d1ec2c3561815beb69a844071b6
edelweiss2 <yunhokim2000@gmail.com>
2022-12-26 16:01:01 +0900

    2022-12-26 Spring

--- src/main/java/com/mulcam/demo/dao/UserDao.java
+++ src/main/java/com/mulcam/demo/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.mulcam.demo.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -12,5 +13,11 @@ public interface UserDao {
 	
 	@Select("select * from users where isDeleted=0")
 	List<User> getList();
+	
+	@Select("select * from users where uid=#{uid}")
+	User get(String uid);
+	
+	@Insert("insert into users values(#{uid}, #{pwd}, #{uname}, #{email}, default, default)")
+	void insert(User u);
 	
 }
