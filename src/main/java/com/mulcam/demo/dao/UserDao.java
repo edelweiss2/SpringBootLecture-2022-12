9fc97ff1a307ada73fc6ca2ada511f087bc0b8f3
edelweiss2 <yunhokim2000@gmail.com>
2022-12-26 14:45:23 +0900

    2002-12-26 Spring

---
+++ src/main/java/com/mulcam/demo/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.mulcam.demo.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.mulcam.demo.entity.User;
+
+@Mapper
+public interface UserDao {
+	
+	@Select("select * from users where isDeleted=0")
+	List<User> getList();
+	
+}
