7baad838f2011082a21233efd55eca40acc73625
edelweiss2 <yunhokim2000@gmail.com>
2023-01-04 20:49:07 +0900

    2023-01-04

--- src/main/java/com/mulcam/demo/controller/MapController.java
+++ src/main/java/com/mulcam/demo/controller/MapController.java
@@ -2,8 +2,12 @@ package com.mulcam.demo.controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mulcam.demo.entity.StaticMap;
+import com.mulcam.demo.service.CsvUtil;
+import com.mulcam.demo.service.MapUtil;
 
 @Controller
 @RequestMapping("/map")
@@ -98,5 +104,102 @@ public class MapController {
 		String roadAddr = (String) jusoItem.get("roadAddr");
 		
 		return sb.toString() + "<br>" + roadAddr;
+	}
+	
+	@ResponseBody
+	@GetMapping("/geocode")
+	public String geocode() throws Exception {
+		String apiUrl = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode";
+		String query = "";
+		query = URLEncoder.encode(query, "utf-8");
+		apiUrl += "?query=" + query;
+		
+		URL url = new URL(apiUrl);
+		// 헤더 설정
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", accessId);
+		conn.setRequestProperty("X-NCP-APIGW-API-KEY", secretKey);
+		conn.setDoInput(true);
+		
+		// 응답 결과 확인
+		int responseCode = conn.getResponseCode();
+		
+		// 데이터 수신
+		BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+		StringBuffer sb = new StringBuffer();
+		String line = null;
+		
+		while((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+		br.close();
+		
+		JSONParser parser = new JSONParser();
+		JSONObject object = (JSONObject) parser.parse(sb.toString());
+		JSONArray addresses = (JSONArray) object.get("addresses");
+		JSONObject address = (JSONObject) addresses.get(0);
+		
+		String lng_ = (String) address.get("x");
+		String lat_ = (String) address.get("y");
+		Double lng = Double.parseDouble(lng_);
+		Double lat = Double.parseDouble(lat_);
+		
+		return "경도: " + lng_ + ", 위도: " + lat_;
+	}
+		
+	@GetMapping("/hotPlaces")
+	public String hotPlaces() throws Exception {
+		String[] hotPlaces = {"대구오페라하우스","대구사격장","어울아트센터","대구엑스코"};
+		String filename = "c:/Temp/지역명소.csv";
+		MapUtil mu = new MapUtil();
+		
+		String output = "";
+		List<List<String>> dataList = new ArrayList<>();
+		for (String place: hotPlaces) {
+			List<String> row = new ArrayList<>();
+			String roadAddr = mu.getRoadAddr(place, roadAddrKey);
+			output += roadAddr + "<br>";
+			List<String> geocode = mu.getGeocode(roadAddr, accessId, secretKey);
+			row.add(place);
+			row.add(roadAddr);
+			row.add(geocode.get(0));		// Longitude(경도)
+			row.add(geocode.get(1)); 		// Latitude(위도)
+			dataList.add(row);
+		}
+		
+		CsvUtil cu = new CsvUtil();
+		cu.writeCsv(filename, dataList);
+		return "redirect:/map/hotPlacesResult";
+	}
+	
+	@GetMapping("/hotPlacesResult")
+	public String hotPlacesResult(Model model) throws Exception {
+		CsvUtil cu = new CsvUtil();
+		List<List<String>> dataList = cu.readCsv("c:/Temp/지역명소.csv");
+		String marker = "";
+		int i = 0;
+		String[] color = {"blue","brown","green","purple"};
+		double lngSum = 0.0, latSum = 0.0;
+		// "type:t|size:tiny|pos:127.0824 37.5383|label:광진구청|color:red"
+		for (List<String> list : dataList)  {
+			double lng = Double.parseDouble(list.get(2));
+			double lat = Double.parseDouble(list.get(3));
+			lngSum += lng; latSum += lat;
+			marker += "&markers=type:t|size:tiny|pos:" + lng + "%20" + lat + "|label:"
+					+ URLEncoder.encode(list.get(0), "utf-8")+ "|color:" + color[i];
+			i++;
+		}
+		
+		double lngCenter = lngSum / dataList.size();
+		double latCenter = latSum / dataList.size();
+		String url = "https://naveropenapi.apigw.ntruss.com/map-static/v2/raster"
+				+ "?w=" + 600 + "&h=" + 400
+				+ "&center=" + lngCenter + "," + latCenter
+				+ "&level=" + 11 + "&scale=" + 2
+				+ "&X-NCP-APIGW-API-KEY-ID=" + accessId
+				+ "&X-NCP-APIGW-API-KEY=" + secretKey;
+		
+		model.addAttribute("url", url+marker);
+		return "map/staticResult";
 	}
 }
